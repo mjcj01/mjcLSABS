15bf4830b251b2599258fc9b8fa35dba0a6122e5
Michael Cattell <mjclego@icloud.com>
2023-11-13 14:15:39 -0500

    Fixed to include all schools in each year they are open.

--- nces_record_linkage_stafford.R
+++ nces_record_linkage_stafford.R
@@ -12,20 +12,38 @@ merge(pair_blocking(stafford_1989 %>% select(ncessch, school_name, street_mailin
                   compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
                                 default_comparator = cmp_jarowinkler(0.9), inplace = TRUE), add = TRUE) %>%
         select_n_to_m(., "weights", variable = "greedy", threshold = 0) %>%
-        link(.),
-      pair_blocking(stafford_1999 %>% select(ncessch, school_name, street_mailing, year), 
-                    stafford_2009 %>% select(ncessch, school_name, street_mailing, year), "ncessch") %>%
-        compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
-                      default_comparator = cmp_jarowinkler(0.9), inplace = TRUE) %>%
-        problink_em(~ ncessch + school_name + street_mailing, data = .) %>%
-        predict(., pairs = pair_blocking(stafford_1999 %>% select(ncessch, school_name, street_mailing, year),
-                                         stafford_2009 %>% select(ncessch, school_name, street_mailing, year),
-                                         "ncessch") %>%
-                  compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
-                                default_comparator = cmp_jarowinkler(0.9), inplace = TRUE), add = TRUE) %>%
-        select_n_to_m(., "weights", variable = "greedy", threshold = 0) %>%
-        link(.) %>% as.data.frame(),
-      by.x = ".y", by.y = ".x", all = TRUE) %>%
+        link(.), 
+      stafford_1999 %>% select(ncessch, school_name, street_mailing), 
+      by.x = c("ncessch.y", "school_name.y", "street_mailing.y"), 
+      by.y = c("ncessch", "school_name", "street_mailing"), all = TRUE) %>% 
+  rename(nces_id_1989 = "ncessch.x",
+         nces_id_1999 = "ncessch.y",
+         school_name_1989 = "school_name.x",
+         school_name_1999 = "school_name.y",
+         street_mailing_1989 = "street_mailing.x",
+         street_mailing_1999 = "street_mailing.y") %>%
+  merge(.,
+        pair_blocking(stafford_1999 %>% select(ncessch, school_name, street_mailing, year), 
+                      stafford_2009 %>% select(ncessch, school_name, street_mailing, year), "ncessch") %>%
+          compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
+                        default_comparator = cmp_jarowinkler(0.9), inplace = TRUE) %>%
+          problink_em(~ ncessch + school_name + street_mailing, data = .) %>%
+          predict(., pairs = pair_blocking(stafford_1999 %>% select(ncessch, school_name, street_mailing, year),
+                                           stafford_2009 %>% select(ncessch, school_name, street_mailing, year),
+                                           "ncessch") %>%
+                    compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
+                                  default_comparator = cmp_jarowinkler(0.9), inplace = TRUE), add = TRUE) %>%
+          select_n_to_m(., "weights", variable = "greedy", threshold = 0) %>%
+          link(.) %>% as.data.frame(),
+        by.x = "nces_id_1999", by.y = "ncessch.x", all = TRUE) %>%
+  select(-school_name.x, -street_mailing.x, -year.x.y) %>%
+  merge(.,
+        stafford_2009 %>% select(ncessch, school_name, street_mailing), 
+        by.x = c("ncessch.y", "school_name.y", "street_mailing.y"), 
+        by.y = c("ncessch", "school_name", "street_mailing"), all = TRUE) %>%
+  rename(nces_id_2009 = "ncessch.y",
+         school_name_2009 = "school_name.y",
+         street_mailing_2009 = "street_mailing.y") %>%
   merge(.,
         pair_blocking(stafford_2009 %>% select(ncessch, school_name, street_mailing, year), 
                       stafford_2019 %>% select(ncessch, school_name, street_mailing, year), "ncessch") %>%
@@ -38,21 +56,19 @@ merge(pair_blocking(stafford_1989 %>% select(ncessch, school_name, street_mailin
                     compare_pairs(., on = c("ncessch", "school_name", "street_mailing"), 
                                   default_comparator = cmp_jarowinkler(0.9), inplace = TRUE), add = TRUE) %>%
           select_n_to_m(., "weights", variable = "greedy", threshold = 0) %>%
-          link %>% as.data.frame(),
-        by.x = ".y.y", by.y = ".y", all = TRUE) %>%
-  select(ncessch.x.x, school_name.x.x, street_mailing.x.x, year.x.x,
-         ncessch.y.x, school_name.y.x, street_mailing.y.x, year.y.x,
-         ncessch.y.y, school_name.y.y, street_mailing.y.y, year.y.y,
-         ncessch.y, school_name.y, street_mailing.y, year.y) %>%
-  rename(nces_id_1989 = "ncessch.x.x",
-         school_name_1989 = "school_name.x.x",
-         street_mailing_1989 = "street_mailing.x.x",
-         nces_id_1999 = "ncessch.y.x",
-         school_name_1999 = "school_name.y.x",
-         street_mailing_1999 = "street_mailing.y.x",
-         nces_id_2009 = "ncessch.y.y",
-         school_name_2009 = "school_name.y.y",
-         street_mailing_2009 = "street_mailing.y.y",
-         nces_id_2019 = "ncessch.y",
+          link(.) %>% as.data.frame(),
+        by.x = "nces_id_2009", by.y = "ncessch.x", all = TRUE) %>%
+  #select(-school_name.x, -street_mailing.x, -year.x) %>%
+  merge(.,
+        stafford_2019 %>% select(ncessch, school_name, street_mailing), 
+        by.x = c("ncessch.y", "school_name.y", "street_mailing.y"), 
+        by.y = c("ncessch", "school_name", "street_mailing"), all = TRUE) %>%
+  rename(nces_id_2019 = "ncessch.y",
          school_name_2019 = "school_name.y",
-         street_mailing_2019 = "street_mailing.y") %>% View()
+         street_mailing_2019 = "street_mailing.y") %>%
+  select(-.y.x, -.x.x, -year.x.x, -year.y.x, -.y.y, -.x.y, -year.y.y, 
+         -.y, -.x, -school_name.x, -street_mailing.x, -year.x, -year.y) %>%
+  select(nces_id_1989, nces_id_1999, nces_id_2009, nces_id_2019,
+         school_name_1989, school_name_1999, school_name_2009, school_name_2019,
+         street_mailing_1989, street_mailing_1999, street_mailing_2009, street_mailing_2019)
+  
