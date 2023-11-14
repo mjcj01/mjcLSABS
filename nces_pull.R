552c8673737c42c9ca89e7af0defee93f6eae9f8
Michael Cattell <mjclego@icloud.com>
2023-11-14 09:53:00 -0500

    Added API pull for all years in one data frame

--- nces_pull.R
+++ nces_pull.R
@@ -1,6 +1,17 @@
 library(educationdata)
 library(tidyverse)
 
+stafford_all <- get_education_data(level = 'schools',
+                                   source = 'ccd',
+                                   topic = 'directory',
+                                   filters = list(year = c(1989, 1999, 2009, 2019),
+                                                  leaid = c('5103660')),
+                                   add_labels = TRUE) %>%
+  mutate("school_name" = gsub("\\.*", "", school_name),
+         "school_name" = gsub("ELEMENTARY", "ELEM", school_name),
+         "street_mailing" = str_to_upper(street_mailing),
+         "street_mailing" = gsub("\\.*", "", street_mailing))
+
 stafford_1989 <- get_education_data(level = 'schools',
                                     source = 'ccd',
                                     topic = 'directory',
